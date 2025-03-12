9a96287d29cd850586668faf769b1c7501dae02b
pduc1234 <78724028+pduc1234@users.noreply.github.com>
2025-03-12 14:25:32 +0700

    Update register-commands.js

--- src/register-commands.js
+++ src/register-commands.js
@@ -7,31 +7,31 @@ const commands = [
         .setDescription("Just say 'Hello!' back"),
     new SlashCommandBuilder()
         .setName("settoken")
-        .setDescription("Thiết lập token của bạn")
+        .setDescription("Set your token")
         .addStringOption(option =>
             option.setName("value").setDescription("Token Hoyolab").setRequired(true)
         ),
     new SlashCommandBuilder()
         .setName("setuid")
-        .setDescription("Thiết lập UID của bạn")
+        .setDescription("Set your UID")
         .addStringOption(option =>
             option.setName("value").setDescription("UID Hoyolab").setRequired(true)
         ),
     new SlashCommandBuilder()
         .setName("checkin")
-        .setDescription("Thực hiện auto check-in Hoyolab"),
+        .setDescription("Auto check-in Hoyolab"),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
     try {
-        console.log("🛠 Đang cập nhật lệnh slash...");
+        console.log("🛠 Updating the slash command ...");
         await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
             body: commands,
         });
-        console.log("✅ Lệnh slash đã được cập nhật!");
+        console.log("✅ Slash command has been updated!");
     } catch (error) {
-        console.error("❌ Lỗi khi đăng ký lệnh:", error);
+        console.error("❌ Error when registering:", error);
     }
 })();
