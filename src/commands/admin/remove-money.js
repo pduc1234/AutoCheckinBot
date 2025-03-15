45be9e9318b918e0ed6500611a071788a3ad888e
pduc1234 <78724028+pduc1234@users.noreply.github.com>
2025-03-15 11:53:31 +0700

    remove-money.js aktualisieren

--- src/commands/admin/remove-money.js
+++ src/commands/admin/remove-money.js
@@ -3,20 +3,19 @@ const economy = loadEconomy();
 
 const handleRemoveMoney = async (interaction) => {
     if (!interaction.member.permissions.has("ADMINISTRATOR"))
-        return interaction.reply("❌ Bạn không có quyền sử dụng lệnh này!");
-
+        return interaction.reply("❌ You do not have the right to use this command!");
     const targetUser = interaction.options.getUser("user");
     const amount = interaction.options.getInteger("amount");
 
     if (!targetUser) {
-        return interaction.reply("❌ Vui lòng chỉ định một người dùng hợp lệ!");
+        return interaction.reply("❌ Please specify a valid user!");
     }    
 
     economy[targetUser.id] = Math.max(0, (economy[targetUser.id] || 0) - amount);
     saveEconomy(economy);
 
     return interaction.reply(
-        `✅ Đã xoá ${amount} <:parallel_coin:1350066344632123462> Coin của <@${targetUser.id}>`,
+        `✅ Removed ${amount} <:parallel_coin:1350066344632123462> Coin of <@${targetUser.id}>`,
     );
 };
 
